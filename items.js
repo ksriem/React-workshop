cec720ade48652ed56be346b4123f01ebfcffa7f
ksriem <76504683+ksriem@users.noreply.github.com>
2021-02-17 17:06:09 +0700

    file react

---
+++ items.js
@@ -0,0 +1,19 @@
+const items =[
+    {
+      title :"man",
+      thumbnaiUrl :"/pic/Koala.jpg"
+    },
+    {
+      title :"gril",
+      thumbnaiUrl :"/pic/Lighthouse.jpeg"
+    },
+    {
+      title :"boy",
+      thumbnaiUrl :"/pic/Penguins.jpg"
+    },
+    {
+      title :"view",
+      thumbnaiUrl :"/pic/Tulips.jpg"
+    }
+  ];
+  export default items;
